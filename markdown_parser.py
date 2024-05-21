574307939a8578c7a44b56fa0ce60bf1bb2b8c3c
daniilchubich <daniilchubich@gmail.com>
2024-05-21 16:20:54 +0300

    test1

--- markdown_parser.py
+++ markdown_parser.py
@@ -6,10 +6,10 @@ def parse_markdown(markdown_text):
     for line in markdown_text.split('\n'):
         if line.startswith('```'):
             in_code_block = not in_code_block
-            if not in_code_block:
-                html_text += "</pre>\n"
-            else:
+            if in_code_block:
                 html_text += "<pre>\n"
+            else:
+                html_text += "</pre>\n"
         elif in_code_block:
             html_text += line + '\n'
         elif line.strip():
@@ -21,7 +21,6 @@ def parse_markdown(markdown_text):
         elif in_paragraph:
             html_text += "</p>\n"
             in_paragraph = False
-        else: print("Помилка")    
 
     if in_paragraph:
         html_text += "</p>\n"
@@ -41,25 +40,22 @@ def apply_formatting(text):
 
 def apply_bold(text):
     while '**' in text:
-        if not  text[2] in "#*_`":
-            text = text.replace('**', '<b>', 1)
-            text = text.replace('**', '</b>', 1)     
+        text = text.replace('**', '<b>', 1)
+        text = text.replace('**', '</b>', 1)
     return text
 
 
 def apply_italic(text):
     while '_' in text:
-        if not  text[2] in "#*_`":
-            text = text.replace('_', '<i>', 1)
-            text = text.replace('_', '</i>', 1)
+        text = text.replace('_', '<i>', 1)
+        text = text.replace('_', '</i>', 1)
     return text
 
 
 def apply_monospace(text):
     while '`' in text:
-        if not  text[2] in "#*_`":
-            text = text.replace('`', '<tt>', 1)
-            text = text.replace('`', '</tt>', 1)
+        text = text.replace('`', '<tt>', 1)
+        text = text.replace('`', '</tt>', 1)
     return text
 
 def main():
@@ -68,15 +64,14 @@ def main():
     while True:
         try:
             path = input()
-            if path[len(path)-3:len(path)] == ".md":
-                file = open(path, "r")
-                #print(file.read())
-                markdown_input = file.read()
-                #print(markdown_text)
+            if path.endswith(".md"):
+                with open(path, "r") as file:
+                    markdown_input = file.read()
                 html_output = parse_markdown(markdown_input)
                 print(html_output)
                 html(html_output)
-            else: print("Incorrect markdown File")
+            else:
+                print("Incorrect markdown File")
         except EOFError:
             break
             
@@ -88,18 +83,16 @@ def html(string):
 </head>
 <body>
 """
-
-    
     html_content += string + "\n"
 
     html_content += """</body>
 </html>
 """
     create_html_file(html_content)    
+    
 def create_html_file(html_content, file_name="output.html"):
-    #os.remove(file_name)
     with open(file_name, "w") as html_file:
         html_file.write(html_content)     
-if __name__ == "__main__":
-   main()
 
+if __name__ == "__main__":
+    main()
